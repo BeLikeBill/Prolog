f0cf924db21698d4d4baeeb9af693aa3a7e77425
mudi-a <moodynn@yahoo.com>
2020-02-23 00:42:42 +0100

    Add files via upload

--- solution3.pl
+++ solution3.pl
@@ -1,8 +1,6 @@
 % Rens Vester
 % Mudi Al-Fageh
 
-:- consult('graph.pl').
-
 
 :- op(100, xfx, at).
 :- op(50, xfx, :).
@@ -10,10 +8,29 @@ route([
     "Amsterdam Centraal" at 11:08,
     "Amsterdam Amstel" at 11:15,
     "Utrecht Centraal" at 11:38,
-    "'s-Hertogenbosch" at 12:08,
+    "s-Hertogenbosch" at 12:08,
     "Eindhoven" at 12:32,
     "Weert" at 12:49,
     "Roermond" at 13:02,
     "Sittard" at 13:21,
     "Maastricht" at 13:35
 ]).
+
+path(From, Time, Path) :-
+    travel(From, Time, Path).
+
+travel(Destination, Destination, _, []).
+travel(From, Time, [route(From, Time, Cost)|Path]) :-
+    route(From, Time, Cost),
+   % travel(Between, To, [Between|Visited], Path).
+
+diffTime(A:B,X:Y, Minutes):-
+    DiffHOURS is X - A,
+    DiffMINUTES is Y - B,
+    Minutes is DiffHOURS*60 + DiffMINUTES,
+    Minutes > 0.
+
+
+/* compare
+earlier(time(H, M1), time(H, M2)) :- !, M1 < M2.
+earlier(time(H1, _), time(H2, _)) :- H1 < H2. */
