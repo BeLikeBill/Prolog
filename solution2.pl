43f5cd2b99cdee3259ea6369140263f0a1db3342
BeLikeBill <rensvester2000@gmail.com>
2020-02-23 00:42:48 +0100

    Add files via upload

--- solution2.pl
+++ solution2.pl
@@ -2,6 +2,7 @@
 % Mudi Al-Fageh
 
 :- consult('graph.pl').
+:- consult('solution1.pl'). %so we can use path(From, To, Cost).
 
 %to get the desired I/O
 cost(Path, TotalCost) :-
@@ -18,27 +19,13 @@ totalcost([Head|Tail], StartCost, TotalCost) :-
     NewCost is StartCost+Cost,
     totalcost(Tail, NewCost, TotalCost).
 
+%to get the path from the ordered tuple in shortest_path
+getpath( (_, P), Path) :-
+    Path = P.
 
 %shortest_path([], EndPath, EndPath).
-shortest_path( From, To, Path) :-
-    traverse(From, To, From, Path, 0, _),
-    findall(Cost, cost(Path, Cost), List),
-    sort(List, List),
-    List = [Head|Tail],
-    Path = Head.
-    %Rule: findall, sort[all costs]
-    %traverse(From, To, 0, Path. 0, LowestCost),
-    %ShortestPath is (min_member(PathCost, TotalCosts)
-    %min_list('Param1', 'Param2') of sort(List) en dan LowestCost = Head of list.
-
-
-
-
-%WIP kant en klare code die alle paden vindt van A naar B en hun kosten.
-%relation all paths and their cost.
-traverse(Result, Result, _, [], EndCost, EndCost).
-traverse(From, To, Visited, [edge(From, Between, Cost)|Path], StartCost, TotalCost) :-
-    edge(From, Between, Cost),
-    \+ member(Between, Visited), % not visited before
-    NewCost is StartCost+Cost, %NewCost is startcost + cost van nieuwe
-    traverse(Between, To, [Between|Visited], Path, NewCost, TotalCost).
+shortest_path(From, To, Path) :-
+    findall((Cost,Path), (path(From, To, Path), cost(Path, Cost)), Bag),
+    sort(Bag, NewBag),
+    NewBag = [Head|_],
+    getpath(Head, Path).
