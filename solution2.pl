70eac8725d66c23268e3ea0060eae0c0490703ef
BeLikeBill <rensvester2000@gmail.com>
2020-02-22 16:34:53 +0100

    Add files via upload

--- solution2.pl
+++ solution2.pl
@@ -3,11 +3,6 @@
 
 :- consult('graph.pl').
 
-shortest_path(From, To, Path) :-
-    traverse(From, To, From, Path, 0, _).
-    %ShortestPath is Path,
-    %ShortestPath is (min_member(PathCost, TotalCosts)
-
 %to get the desired I/O
 cost(Path, TotalCost) :-
     totalcost(Path, 0, TotalCost).
@@ -24,6 +19,22 @@ totalcost([Head|Tail], StartCost, TotalCost) :-
     totalcost(Tail, NewCost, TotalCost).
 
 
+%shortest_path([], EndPath, EndPath).
+shortest_path( From, To, Path) :-
+    traverse(From, To, From, Path, 0, _),
+    findall(Cost, cost(Path, Cost), List),
+    sort(List, List),
+    List = [Head|Tail],
+    Path = Head.
+    %Rule: findall, sort[all costs]
+    %traverse(From, To, 0, Path. 0, LowestCost),
+    %ShortestPath is (min_member(PathCost, TotalCosts)
+    %min_list('Param1', 'Param2') of sort(List) en dan LowestCost = Head of list.
+
+
+
+
+%WIP kant en klare code die alle paden vindt van A naar B en hun kosten.
 %relation all paths and their cost.
 traverse(Result, Result, _, [], EndCost, EndCost).
 traverse(From, To, Visited, [edge(From, Between, Cost)|Path], StartCost, TotalCost) :-
