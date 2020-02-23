1e2f8cad54c5ebe3102f160e3e66dc08bdd6a96b
BeLikeBill <rensvester2000@gmail.com>
2020-02-23 04:42:03 +0100

    Add files via upload

--- solution3.pl
+++ solution3.pl
@@ -1,14 +1,14 @@
 % Rens Vester
 % Mudi Al-Fageh
 
-
 :- op(100, xfx, at).
 :- op(50, xfx, :).
+
 route([
     "Amsterdam Centraal" at 11:08,
     "Amsterdam Amstel" at 11:15,
     "Utrecht Centraal" at 11:38,
-    "s-Hertogenbosch" at 12:08,
+    "'s-Hertogenbosch" at 12:08,
     "Eindhoven" at 12:32,
     "Weert" at 12:49,
     "Roermond" at 13:02,
@@ -16,21 +16,20 @@ route([
     "Maastricht" at 13:35
 ]).
 
-path(From, Time, Path) :-
-    travel(From, Time, Path).
+%gets the shortest path between two stations
+shortestPath(Destination, Destination, []).
+shortestPath(From at _, To at _, [NewPath|Path]) :-
+    route(Stations),
+    %member(To at End, Stations),
+    member(From at Start, Stations),
+    member(Between at Inter, Stations),
+    diffTime(Start, Inter, Time),
+    NewPath = travel(From at Start, Between at Inter, Time),
+    shortestPath(Between at Inter, To at _, Path), !.
 
-travel(Destination, Destination, _, []).
-travel(From, Time, [route(From, Time, Cost)|Path]) :-
-    route(From, Time, Cost),
-   % travel(Between, To, [Between|Visited], Path).
-
-diffTime(A:B,X:Y, Minutes):-
+%calculate the minutes between two times
+diffTime(A:B,X:Y, Minutes) :-
     DiffHOURS is X - A,
     DiffMINUTES is Y - B,
     Minutes is DiffHOURS*60 + DiffMINUTES,
     Minutes > 0.
-
-
-/* compare
-earlier(time(H, M1), time(H, M2)) :- !, M1 < M2.
-earlier(time(H1, _), time(H2, _)) :- H1 < H2. */
