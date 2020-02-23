6ee1d7e2313b3df41b15a69fe1c5bb233ce59611
Rens Vester <rensvester2000@gmail.com>
2020-02-23 11:30:23 +0100

    solution3 right

--- team/solution3.pl
+++ team/solution3.pl
@@ -4,17 +4,7 @@
 :- op(100, xfx, at).
 :- op(50, xfx, :).
 
-route([
-    "Amsterdam Centraal" at 11:08,
-    "Amsterdam Amstel" at 11:15,
-    "Utrecht Centraal" at 11:38,
-    "'s-Hertogenbosch" at 12:08,
-    "Eindhoven" at 12:32,
-    "Weert" at 12:49,
-    "Roermond" at 13:02,
-    "Sittard" at 13:21,
-    "Maastricht" at 13:35
-]).
+:- consult('route.pl').
 
 %gets the shortest path between two stations
 shortestPath(Destination, Destination, []).
