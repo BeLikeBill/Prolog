9e2c2a2bf4bc5c90ead55c751ed09461c855f455
BeLikeBill <rensvester2000@gmail.com>
2020-02-23 16:15:52 +0100

    Add files via upload

--- team/queries.pl
+++ team/queries.pl
@@ -1,9 +1,13 @@
 % Rens Vester - s12958042
 % Mudi Al-Fageh
 
-:- consult('solution2.pl').
+%
+% Queries for the questions in report.pdf
+%
 
 %for solution 2.1 finding the cost of all paths from 5 to 4
 %gets the shortest path from to
-getallCosts(From, To, Costs) :-
-    findall((Cost, Path), (path(From, To, Path), cost(Path, Cost)), Costs).
+?- findall((Path, Cost), (path(5, 4, Path), cost(Path, Cost)), Costs).
+
+%for solution 3.1 finding the duration of getting from Amstel to Sittard
+?- shortestPath("Amsterdam Amstel" at Start, "Sittard" at _, Path), cost(Path, TotalCost).
